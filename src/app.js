952c15ae6bc267516f8afbe2ab290330d2482577
Yash <yaswanth.chundru@gmail.com>
2025-11-19 12:02:27 +0530

    Creating a Server on a specifc Port

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
 const express = require("express")
+const dbConnect = require("./config/database")
+const app = express()
+
+app.use(express.json())
+
+dbConnect()
+    .then( () =>{
+        console.log("Database Connection Successful!")
+        app.listen(5000, () => {
+            console.log("Successfully listening on PORT num 5000")
+        })
+    } )
+    .catch( (err) => {
+        console.log("Database connection is not successful " + err.message)
+    } )
