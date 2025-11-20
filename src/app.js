8ef6d2e236093348be754afa15cf73c40c3bc3be
Yash <yaswanth.chundru@gmail.com>
2025-11-20 11:46:50 +0530

    Dynamic Port

--- src/app.js
+++ src/app.js
@@ -8,14 +8,17 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser());
 
+
 app.use("/",authRouter)
 app.use("/",profileRouters)
+
+const PORT = process.env.PORT || 3000;
 
 dbConnect()
     .then( () =>{
         console.log("Database Connection Successful!")
-        app.listen(5000, () => {
-            console.log("Successfully listening on PORT num 5000")
+        app.listen(PORT, () => {
+            console.log("Successfully listening on PORT num: ", PORT)
         })
     } )
     .catch( (err) => {
