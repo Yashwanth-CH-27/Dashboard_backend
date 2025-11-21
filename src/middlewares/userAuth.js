bc3cef5aea649d8606b95a8e4d08eaf59ec67068
Yash <yaswanth.chundru@gmail.com>
2025-11-21 11:47:15 +0530

    bug fix1

--- src/middlewares/userAuth.js
+++ src/middlewares/userAuth.js
@@ -1,24 +1,32 @@
-const jwt = require("jsonwebtoken")
-const User = require("../models/user")
-require("dotenv").config()
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+require("dotenv").config();
 
-const userAuth = async (req,res,next) =>{
-    try{
-        const {token} = req.cookies
-        if(!token){
-            return res.status(401).send("Please SignIn!");
-        }
-        const decodeToken = await jwt.verify(token, process.env.JWT_SECRET, {expiresIn: "3d"})
-        const {_id} = decodeToken
-        const user = await User.findOne({_id})
-        if(!user){
-            throw new Error("User not found!")
-        }
-        req.user = user
-        next()
-    }catch(err){
-        res.send(err.message)
+const userAuth = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      return res.status(401).send("Please SignIn!");
     }
-}
 
-module.exports = userAuth
+    const decodeToken = await jwt.verify(
+      token,
+      process.env.JWT_SECRET,
+      { expiresIn: "3d" }
+    );
+    const { _id } = decodeToken;
+    const user = await User.findOne({ _id });
+
+    if (!user) {
+      return res.status(404).send("User not found!");
+    }
+
+    req.user = user;
+    next();
+  } catch (err) {
+    console.error("Auth error:", err);
+    return res.status(401).send(err.message || "Unauthorized");
+  }
+};
+
+module.exports = userAuth;
